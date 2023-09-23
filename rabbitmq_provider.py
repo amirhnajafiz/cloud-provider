2b2653d2dc47f47e8b433061e815a18a88b97561
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-23 15:40:58 +0330

    add: rabbit mq provider

---
+++ rabbitmq_provider.py
@@ -0,0 +1,13 @@
+import pika
+
+
+QUEUE_NAME = 'vm-queue'
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue=QUEUE_NAME)
+
+channel.basic_publish(
+    exchange='', routing_key=QUEUE_NAME, body='one VM please ^_^')
