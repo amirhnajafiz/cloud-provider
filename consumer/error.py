632a169e96e2e5e26c056f525b6239b40b198801
amirhnajafiz <najafizadeh21@gmail.com>
2023-09-30 16:56:13 +0330

    add: error handler in create user image

---
+++ consumer/error.py
@@ -0,0 +1,2 @@
+class QemuException(Exception):
+    pass
--- consumer/main.py
+++ consumer/main.py
@@ -1,15 +1,44 @@
 import pika
+from pathlib import Path
+import subprocess
+
+from error import QemuException
 
 
+# rabbitMQ topic name
 QUEUE_NAME = 'vm-queue'
+MQTT_HOST = 'localhost'
 
+BASE_IMAGE_FOLDER = Path('base_images')
+USER_IMAGE_FOLDER = Path('user_images')
+
+# open rabbitmq connections
 connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='localhost'))
+    pika.ConnectionParameters(host=MQTT_HOST))
+
 channel = connection.channel()
 
 channel.queue_declare(queue=QUEUE_NAME)
 
 
+# create a new vm by using the user requested image
+def create_vm(vm_id: str, image_name: str) -> Path:
+    base_image = BASE_IMAGE_FOLDER / f'{image_name}.qcow2'
+    if not base_image.is_file():
+        raise IOError(f'Image "{image_name}" does not exist')
+
+    user_image = USER_IMAGE_FOLDER / f'{vm_id}.qcow2'
+
+    create_img_result = subprocess.run([
+        'qemu-img', 'create', '-f', 'qcow2',
+        '-b', str(base_image.absolute()), '-F', 'qcow2', str(user_image)])
+    if create_img_result.returncode != 0:
+        raise QemuException(f'Could not create image for VM "{vm_id}"')
+
+    return user_image
+
+
+# call back method for rabbitmq consumer
 def callback(ch, method, properties, body):
     print('Received message: ' + body.decode('utf-8'))
     ch.basic_ack(delivery_tag=method.delivery_tag)
