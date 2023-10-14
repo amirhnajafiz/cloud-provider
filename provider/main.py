c4f739a7b698c593ecf13870ab27ba225e41ea4a
amirhnajafiz <najafizadeh21@gmail.com>
2023-10-14 11:22:19 +0330

    add: server communication in client cli

--- provider/main.py
+++ provider/main.py
@@ -6,24 +6,27 @@ import json
 QUEUE_NAME = 'vm-queue'
 MQTT_HOST = 'localhost'
 
-# opening a connection to rabbit channel
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host=MQTT_HOST))
 
-# connecting to provided channel
-channel = connection.channel()
+class ServerCommunication:
+    def __init__(self):
+        self.queue = QUEUE_NAME
 
-# create a queue
-channel.queue_declare(queue=QUEUE_NAME)
+    def __enter__(self):
+        # opening a connection to rabbit channel
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=MQTT_HOST))
 
-# message to send
-message = json.dumps({
-    'command': 'start-vm',
-    'options': {
-        'image': 'ubuntu_base',
-    }
-})
+        # connecting to provided channel
+        self.channel = self.connection.channel()
 
-# publish message
-channel.basic_publish(
-    exchange='', routing_key=QUEUE_NAME, body=bytes(message, "utf-8"))
+        # create a queue
+        self.channel.queue_declare(queue=QUEUE_NAME)
+
+        # create a basic consume for response handler
+        self.channel.basic_consume(
+            queue='amq.rabbitmq.reply-to',
+            on_message_callback=self.on_response,
+            auto_ack=True
+        )
+
+    def on_response(self, ch, method, properties, body):
+        pass
